1f5dded3faa961491e81cfb89f7faf348cd80ebf
fernando <fernando@juandemariana.org>
2025-03-14 23:59:05 +0100

    Add api/v1/yourip route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/app-name', function () {
+Route::get('/v1/appname', function () {
     return response()->json([
         'app_name' => config('app.name'),
     ]);
+});
+Route::get('/v1/yourip', function () {
+    return response()->json(['ip' => request()->ip()]);
 });
