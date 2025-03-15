665540d8dba49c1b5a712c51a4279b5aea4a9352
fernando <fernando@juandemariana.org>
2025-03-15 23:34:19 +0100

    Add datetime at routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
+Route::get('/v1/datetime', function () {
+    return response()->json(['datetime' => now()->toDateTimeString()]);
+});
+
 Route::get('/v1/{info}', [ApiController::class, 'getInfo']);
+
+
