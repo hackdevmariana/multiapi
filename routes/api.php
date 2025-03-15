be4f8590b5782580b343985e7a36361109a10fdb
fernando <fernando@juandemariana.org>
2025-03-15 23:38:29 +0100

    Add generate-password at routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,12 @@ Route::get('/v1/check', function () {
     return response()->json(['status' => 'OK']);
 });
 
+Route::get('/v1/generate-password', function (Illuminate\Http\Request $request) {
+    $length = $request->query('length', 12); // Longitud por defecto: 12 caracteres
+    $password = Str::random($length);
+    return response()->json(['password' => $password]);
+});
+
 
 Route::get('/v1/uuid', function () {
     return response()->json(['uuid' => Str::uuid()]);
@@ -17,5 +23,4 @@ Route::get('/v1/datetime', function () {
 });
 
 Route::get('/v1/{info}', [ApiController::class, 'getInfo']);
-
 
