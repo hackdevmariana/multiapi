58c9b52f039d10e7bf46f2812b37365043e3addf
fernando <fernando@juandemariana.org>
2025-04-05 22:45:27 +0200

    Add generateRandomPalette function in ColormindController

--- app/Http/Controllers/ColormindController.php
+++ app/Http/Controllers/ColormindController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ColormindController extends Controller
 {
-    //
+    // Generar paleta aleatoria
+    public function generateRandomPalette()
+    {
+        $response = Http::post(env('COLORMIND_API_URL'), [
+            'model' => 'default', // Modelo por defecto
+        ]);
+
+        if ($response->ok()) {
+            return response()->json([
+                'palette' => $response->json()['result'],
+            ]);
+        }
+
+        return response()->json(['error' => 'No se pudo generar la paleta de colores.'], $response->status());
+    }
+
 }
